a70615b43dcb9cfd557b3a76b9b8412543145130
Mussie <mussie.beian@zerog.aero>
2020-03-21 19:03:57 +0100

    bugfixes

--- covidash/data/preprocessing.py
+++ covidash/data/preprocessing.py
@@ -10,18 +10,19 @@ def read_json_data(json_file):
 
 def get_hospital_list():
     """"reads a json extracts hospital info and merges it with lat long coordinates"""
-    json_hospital = read_json_data("data/raw/osm_hospital_locations_germany.json")
+    json_hospital = read_json_data("../../data/raw/osm_hospital_locations_germany.json")
+    features = json_hospital['features']
     df_hospital_properties = pd.DataFrame([item['properties'] for item in features])
     df_hospital_points = pd.DataFrame([item['geometry']['coordinates'] for item in features])
 
-    df_hospitals = df_hospital_properites.join(df_hospital_points)
+    df_hospitals = df_hospital_properties.join(df_hospital_points)
     return df_hospitals.rename(columns={0:"x",1:"y"})
 
-df_covid = pd.read_csv('data/raw/rki_covid_19.csv')
+df_covid = pd.read_csv('../../data/raw/rki_covid_19.csv')
 
-df_landkreise = pd.read_csv('data/raw/rki_corona_landkreise.csv')
+df_landkreise = pd.read_csv('../../data/raw/rki_corona_landkreise.csv')
 
 df_hospital = get_hospital_list()
 
-
+print('1')
 
