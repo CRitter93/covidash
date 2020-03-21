951db4818a89b460b2870c01d57788b3cf3f8c13
Mussie <mussie.beian@zerog.aero>
2020-03-21 18:50:24 +0100

    preprocessing of hospital geo data

--- covidash/data/download.py
+++ covidash/data/download.py
@@ -11,20 +11,17 @@ def get_rki_data(url="", file_name=""):
         urllib.request.urlretrieve(url, file_name)
 
 
-def download_osm_hospital_locations():
-    get_rki_data(url="https://opendata.arcgis.com/datasets/348b643c8b234cdc8b1b345210975b87_0.geojson",
-                 file_name="osm_hospital_locations_germany.json")
-
-
 def download_rki_covid_19():
     get_rki_data(url='https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv',
                  file_name='rki_covid_19.csv')
 
+def download_osm_hospital_locations():
+    get_rki_data(url="https://opendata.arcgis.com/datasets/348b643c8b234cdc8b1b345210975b87_0.geojson",
+                 file_name="osm_hospital_locations_germany.json")
 
 def download_rki_corona_landkreise():
     get_rki_data(url='https://opendata.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0.csv',
                  file_name='rki_corona_landkreise.csv')
-
 
 def download_rki_corona_laendergrenzen():
     "Data Source https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/esri-de-content::bundesl%C3%A4ndergrenzen-2019/geoservice"
@@ -32,7 +29,9 @@ def download_rki_corona_laendergrenzen():
                  file_name='bundeslaender_grenzen_2019.geojson')
 
 
+
 if __name__ == '__main__':
     download_rki_covid_19()
-    download_rki_corona_landkreise()
     download_osm_hospital_locations()
+    download_rki_corona_landkreise()
+    download_rki_corona_laendergrenzen()
---
+++ covidash/data/preprocessing.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import json
+import numpy as np
+
+
+def read_json_data(json_file):
+    with open(json_file) as json_file:
+        json_data = json.load(json_file)
+    return json_data
+
+def get_hospital_list():
+    """"reads a json extracts hospital info and merges it with lat long coordinates"""
+    json_hospital = read_json_data("data/raw/osm_hospital_locations_germany.json")
+    df_hospital_properties = pd.DataFrame([item['properties'] for item in features])
+    df_hospital_points = pd.DataFrame([item['geometry']['coordinates'] for item in features])
+
+    df_hospitals = df_hospital_properites.join(df_hospital_points)
+    return df_hospitals.rename({0:"x",1:"y"},axis=1)
+
+df_covid = pd.read_csv('data/raw/rki_covid_19.csv')
+
+df_landkreise = pd.read_csv('data/raw/rki_corona_landkreise.csv')
+
+df_hospital = get_hospital_list()
+
+
+
