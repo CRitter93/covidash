154a2fef2ffc4f501fa7cdd655479f302ad42bc1
Mussie <mussie.beian@zerog.aero>
2020-03-21 13:41:47 +0100

    add bundeslaender grenzen

--- covidash/data/download.py
+++ covidash/data/download.py
@@ -16,6 +16,12 @@ def download_rki_corona_landkreise():
     urllib.request.urlretrieve(url, file_name)
 
 
+def download_rki_corona_landkreise():
+    "Data Source https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/esri-de-content::bundesl%C3%A4ndergrenzen-2019/geoservice"
+    url = 'https://opendata.arcgis.com/datasets/9ae4f23075d340adb6580a6d9603f9fa_0.geojson'
+    file_name = os.path.join(DATA_DIR, 'bundeslaender_grenzen_2019.geojson')
+
+
 if __name__ == '__main__':
     download_rki_covid_19()
     download_rki_corona_landkreise()
