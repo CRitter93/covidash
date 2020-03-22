7f71f4f771800cbf7d15143f7d4645e357ccbd98
Mussie <mussie.beian@zerog.aero>
2020-03-22 01:12:17 +0100

    add states to hospital geojson

--- covidash/data/preprocessing.py
+++ covidash/data/preprocessing.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import json
 import numpy as np
-
+import pprint
+#from shapely.geometry import Polygon, Point
+import geopandas as gpd
+from tqdm import tqdm
 
 def read_json_data(json_file):
     with open(json_file) as json_file:
         json_data = json.load(json_file)
     return json_data
+
+def pretty(d, indent=0):
+   for key, value in d.items():
+      print('\t' * indent + str(key))
+      if isinstance(value, dict):
+         pretty(value, indent+1)
+      else:
+         print('\t' * (indent+1) + str(value))
 
 def get_hospital_list():
     """"reads a json extracts hospital info and merges it with lat long coordinates"""
@@ -20,8 +31,37 @@ def get_hospital_list():
 
 df_covid = pd.read_csv('../../data/raw/rki_covid_19.csv')
 
-df_landkreise = pd.read_csv('../../data/raw/rki_corona_landkreise.csv')
+df__rki_landkreise = pd.read_csv('../../data/raw/rki_corona_landkreise.csv')
 
 df_hospital = get_hospital_list()
 
+gdf_states = gpd.read_file("../../data/raw/bundeslaender_grenzen_2019.geojson")
 
+gdf_hospitals = gpd.read_file("../../data/raw/osm_hospital_locations_germany.json").drop([166, 284, 285, 291])
+
+
+hospital_state_map = {}
+for idx, coord in gdf_hospitals.iterrows():
+    for _idx, _row in  gdf_states.iterrows():
+        if coord['geometry'].within(_row.geometry):
+            hospital_state_map[idx] = _row.LAN_GEN
+
+gdf_hospitals['hospital_index'] = gdf_hospitals.index.tolist()
+
+def hosp_index_to_state(state_map,item_idx):
+    try:
+        state_name = state_map[item_idx]
+        return state_name
+    except KeyError:
+        return np.nan
+
+gdf_hospitals['state_name'] = gdf_hospitals.hospital_index.apply(lambda row: hosp_index_to_state(hospital_state_map,row))
+
+gdf_hospitals.to_json("../../data/processed/osm_hospital_locations_germany_with_States.json")
+
+
+
+
+
+
+print("1")
